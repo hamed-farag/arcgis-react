fcf6415db3c59e74391a4b46e92420ee831d61b8
Hamed Farag <hamed@wakecap.com>
2023-04-17 12:42:38 +0400

    chore: update readme file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import EsriConfig from "@arcgis/core/config";
 
 import { ThreeDMap } from "./components/ThreeDMap";
-// import { DrawPolygon } from "./components/DrawPolygon";
+import { DrawPolygon } from "./components/DrawPolygon";
 
 import "./app.css";
 
 function App() {
-  EsriConfig.apiKey = import.meta.env.VITE_ARCHGIS_API_KEY;
+  EsriConfig.apiKey = import.meta.env.VITE_ARCGIS_API_KEY;
 
   const configs = {
     center: [55.18121, 25.034787],
@@ -15,13 +15,19 @@ function App() {
 
   return (
     <div>
-      <ThreeDMap {...configs} />
-      {/* <DrawPolygon
-        {...configs}
-        onComplete={(points) => {
-          console.info("points", points);
-        }}
-      /> */}
+      <div>
+        <h1>3D Map</h1>
+        <ThreeDMap {...configs} />
+      </div>
+      <div>
+        <h1>Draw Polygon</h1>
+        <DrawPolygon
+          {...configs}
+          onComplete={(points) => {
+            console.info("points", points);
+          }}
+        />
+      </div>
     </div>
   );
 }
